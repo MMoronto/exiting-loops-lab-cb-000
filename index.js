78dd8c7eabe76b458cc0bcca09bb7f10d26c05fb
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-10-01 19:47:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-function breakOut(array, changeValue, stopValue) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === stopValue) {
-      break; 
-    }
-    array[i] = changeValue
-  }
-  return array
-}
+function breakOut(array, changeValue, stopValue) 
