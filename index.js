7c9691b7107ef876745455daccf36c0e0ec6601c
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-10-01 20:09:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn) {
-  for (let i = 0, l = array.length; i < l; i++) 
-  
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (findFn(array[i])) {
+      return array[i]
+    }
+  }
+
+  return null
 }
